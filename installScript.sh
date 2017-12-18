c6d3a0eb14c6cbbb51655a501d0d48030894c340
NickGraeff <nicksnacs@gmail.com>
2017-12-17 23:25:08 -0800

    Added debuggers I use

--- installScript.sh
+++ installScript.sh
@@ -1,6 +1,8 @@
 sudo apt-get install terminator
 sudo apt-get install git
 sudo apt-get install vim
+sudo apt-get install ddd
+sudo apt-get install cgdb
 git clone https://www.github.com/NickGraeff/vimrc.git
 cd vimrc
 cp -r .vim ../
