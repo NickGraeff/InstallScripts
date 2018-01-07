c2b14d86849e85abdc68dd1075bb16a2d9b3e366
NickGraeff <nicksnacs@gmail.com>
2018-01-06 20:54:27 -0800

    Updated vim

--- installScript.sh
+++ installScript.sh
@@ -1,6 +1,6 @@
 sudo apt-get install terminator
 sudo apt-get install git
-sudo apt-get install vim
+sudo apt-get install vim-gtk
 sudo apt-get install ddd
 sudo apt-get install cgdb
 git clone https://www.github.com/NickGraeff/vimrc.git
