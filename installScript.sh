a80f9a726c661ff4b9183555e6717ad071a25d79
nickgraeff <nicksnacs@gmail.com>
2018-02-03 00:14:02 -0800

    asdf

--- installScript.sh
+++ installScript.sh
@@ -3,6 +3,9 @@ sudo apt-get install git
 sudo apt-get install vim-gtk
 sudo apt-get install ddd
 sudo apt-get install cgdb
+sudo apt-get install python-pip
+sudo apt-get install python-matplotlib
+sudo apt-get install python-numpy
 git clone https://www.github.com/NickGraeff/vimrc.git
 cd vimrc
 cp -r .vim ../
